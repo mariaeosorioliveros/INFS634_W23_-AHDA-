808c9e1b51bc6731c089d468c253ebfe9092c0d4
mariaeosorioliveros <maria.osorio@mail.mcgill.ca>
2023-03-24 20:26:32 -0400

    update 24-03

--- footer.php
+++ footer.php
@@ -1,10 +1,13 @@
 <!--This is AHDA footer section-->
 <footer>
-<div class="container-fluid" style="background-color: rgba(92, 126, 102, 1);">  
-<img src="/assets/img/footerlogo.jpg" class="mx-auto d-block" alt="ahda-footer-logo" width="100" height="auto">	
+	<div class="container-fluid" style="background-color: rgba(92, 126, 102, 1);">  
+	<img src="/assets/img/footerlogo.jpg" class="mx-auto d-block" alt="ahda-footer-logo" width="100" height="auto">	
+	</div>
+	
+	<div>
 	<div class= "row" style="background-color:rgba(25, 51, 39, 1);">
 		<p id= "footer-line">Created by Maria Eugenia Osorio Oliveros, Montréal, Québec, 2023.</p>		
-</div>
+	</div>
 </footer>	
 
 

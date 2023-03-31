c7f0e8444c07004da428b48e585c7612bbcada9e
mariaeosorioliveros <maria.osorio@mail.mcgill.ca>
2023-03-31 16:47:28 -0400

    update 31-0.2

--- ahda-header.php
+++ ahda-header.php
@@ -2,19 +2,18 @@
   <div class="container-fluid p-3" style="background-color:rgb(230,241,201);">
       <img src="/ahda_assets/img/logoheader.jpg" class="mx-auto d-block" alt="ahdaheaderlogo" width="290" height="auto">
       <h2 id= "logo-header" class="fw-bold">AMERICAN HERBOLARIA DIGITAL ARCHIVE </h2>
+      </div>
+</header>      
 <!--This is AHDA navbar and dropdown menu-->
-    <nav class="navbar navbar-expand-lg"style="background-color:rgb(230,241,201);">   
-	  
+  <nav class="navbar navbar-expand-lg"style="background-color:rgb(230,241,201);">   
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     	</button>
-    
       <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#"><img src="/assets/img/home-svgrepo-com.svg" alt="house-icon" width="25" height="auto" style= "vertical-align:baseline"></a>
+                <a class="nav-link active" aria-current="page" href="#"><img src="/ahda_assets/img/home-svgrepo-com.svg" alt="house-icon" width="25" height="auto" style= "vertical-align:baseline"></a>
             </li> 
-
            <li class="nav-item dropdown dropdown-mega position-static">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             About
@@ -28,7 +27,6 @@
                   </ul> 
               </div>
            </li>   
-
           <li class="nav-item dropdown dropdown-mega position-static">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Archive
@@ -42,7 +40,6 @@
               </ul> 
               </div>
           </li>    
-
           <li class="nav-item dropdown dropdown-mega position-static">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Herbolaria Collections
@@ -53,8 +50,7 @@
             </ul>            
             </div>
           </li>  
-
-          <li class="nav-item dropdown dropdown-mega position-static">
+        <li class="nav-item dropdown dropdown-mega position-static">
           <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Projects
           </a>
@@ -63,18 +59,15 @@
               <li><a class="dropdown-item" href="#">Herbolaria Projects</a></li>
             </ul>            
             </div>
-          </li>
-          
+          </li> 
           <li class="nav-item">
             <a class="nav-link" href="#">Contact</a>
           </li>
-
           <li class="nav-item">
-            <a class="nav-link" href="#"><img src="/assets/img/leaf-svgrepo-com.svg" alt="leaf-icon" width="25" height="auto" style= "vertical-align:top">Español</a>
+            <a class="nav-link" href="#"><img src="/ahda_assets/img/leaf-svgrepo-com.svg" alt="leaf-icon" width="25" height="auto" style= "vertical-align:top">Español</a>
           </li>
       </ul>
     </div>
   </nav>
-  </div>
-</header>
+  
 

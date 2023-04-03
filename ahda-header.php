141e851710e8578567e1f19c3f51d01b9e2443ba
mariaeosorioliveros <maria.osorio@mail.mcgill.ca>
2023-04-02 22:27:33 -0400

    update 02-04

--- ahda-header.php
+++ ahda-header.php
@@ -1,13 +1,74 @@
 <header>
-  <div class="container-fluid p-3" style="background-color:rgb(230,241,201);">
+<div class="container-fluid p-4" style="background-color:rgb(230,241,201);">
   <img src="/ahda_assets/img/logoheader.jpg" class="mx-auto d-block" alt="ahdaheaderlogo" width="290" height="auto">
-  <h1 id= "logo-title" class="fw-bold">AMERICAN HERBOLARIA DIGITAL ARCHIVE </h1>
+  <figcaption id= "logo-title" class="fw-bold">AMERICAN HERBOLARIA DIGITAL ARCHIVE </figcaption> 
   </div>
-</header>        
-<!--This is AHDA navbar and dropdown menu-->
+</header>               
+<!--This is ahda navbar and dropdown menu-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 justify-content-center">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            About
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <ul>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+          </ul>  
+          <ul>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          </ul>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Archive
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <ul>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+          </ul>  
+          <ul>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          </ul>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Herbolaria Collections
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <ul>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+          </ul>  
+          <ul>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          </ul>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           Projects
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <ul>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+          </ul>  
+          <ul>
+          <li><a class="dropdown-item" href="#">Another action</a></li>
+          </ul>
+          </div>
+        </li>
 
-     
-
-  
-  
-
+      </ul>
+    </div>
+  </div>
+</nav>

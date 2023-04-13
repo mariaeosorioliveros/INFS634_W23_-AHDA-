ff5b06d30129ec5dca193f914aeeca8445af289b
mariaeosorioliveros <maria.osorio@mail.mcgill.ca>
2023-04-13 16:27:30 -0400

    13/04

---
+++ ahda-Archive.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="author" content="Maria Eugenia Osorio Oliveros">
+ <title>AHDA:Archive</title>
+ <link rel= "icon" type="image/x-icon" href="/ahda_assets/img/logobookmarkwb.png">
+ <link rel="stylesheet" type="text/css" href="/ahda_assets/css/Mystyle1-ahda.css"> 
+ <link rel="stylesheet" href="/ahda_assets/css/bootstrap.css"/>
+ <link rel= "stylesheet" href="https://fonts.googleapis.com/css2?family=Catamaran:wght@300&family=Cormorant+Garamond&family=Roboto&display=swap"> 
+ <!--Bootstrap 5.CSS link--> 
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+ <!--Boostrap 5-Icons link-->
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+</head>
+
+<body>
+  <?php include ('ahda-header.php');?>
+    <main>
+
+    </main>
+</body>
+</html>
